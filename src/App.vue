bf68fae3e2bebaa94140217f60121c2dae413bc4
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-02-28 18:53:46 +0100

    Basics of app header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <script lang="ts" setup>
-import sejm2023 from '@/data/pl/sejm/2023';
-
+import AppHeader from '@/components/AppHeader.vue';
 </script>
 
 <template>
-  <header>
-    Main App
-  </header>
-
   <main>
-    {{ sejm2023 }}
+    <AppHeader />
+    <router-view />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+html, body, main {
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+  Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 15px;
+  background-color: white;
+  padding: 0;
+  margin: 0;
+  color: black;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <header>
+    <h1>
+      Elections Switcher
+    </h1>
+  </header>
+</template>
+
+<style>
+header {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+  text-align: center;
+  width: 100%;
+}
+
+h1 {
+  margin: 0;
+  font-size: 1.5rem;
+}
+</style>
