9b9569f34629f9f72560a2d2377a2ff88a1edb52
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-03-01 13:00:47 +0100

    Color updates

--- src/components/pl/sejm/NumberOfSeatsPicker.vue
+++ src/components/pl/sejm/NumberOfSeatsPicker.vue
@@ -17,7 +17,7 @@ watch(() => numberOfSeats.value, (newValue) => {
 <template>
   <div>
     <label for="numberOfSeats">Number of seats:</label>
-    <input id="numberOfSeats" type="number" v-model="numberOfSeats" :min="1" />
+    <input id="numberOfSeats" type="number" v-model="numberOfSeats" :min="1" :max="99999" />
     <div v-if="showRealSeatsToggle && defaultNumberOfSeats === numberOfSeats">
       <label for="useRealSeats">Use real seats:</label>
       <input id="useRealSeats" type="checkbox" v-model="useRealSeats" :key="useRealSeats ? 'yes' : 'no'" />
