b8a0b6c54fa48e9c044561a61c83bf891a3824ea
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-03-01 12:24:21 +0100

    Refactor district magnitude configuration to a separate component

---
+++ src/components/pl/sejm/DistrictMagnitudeConfiguration.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import { Sejm } from '@/models/pl/sejm';
+import { NumberMap } from '@/models/utils/number-map';
+import { computed, ref, watch } from 'vue';
+import { getDistrictMagnitude } from '@/utils/pl/district-magnitude';
+import NumberOfSeatsPicker from '@/components/pl/sejm/NumberOfSeatsPicker.vue';
+
+const props = defineProps<{
+  data: Sejm
+}>();
+
+const model = defineModel<NumberMap>();
+
+const mandateOverrides = computed<NumberMap>(() => {
+  return props.data.districtResults.reduce((acc, districtResult) => {
+    if (districtResult.numberOfMandatesUsed) {
+      acc[districtResult.districtNumber] = districtResult.numberOfMandatesUsed;
+    }
+    return acc;
+  }, {} as NumberMap);
+});
+
+const populationMap = computed<NumberMap>(() => {
+  return props.data.districtResults.reduce((acc, districtResult) => {
+    acc[districtResult.districtNumber] = districtResult.population;
+    return acc;
+  }, {} as NumberMap);
+});
+
+const seatDistribution = computed<NumberMap>(() => {
+  return getDistrictMagnitude(
+      populationMap.value,
+      useRealSeats.value ? mandateOverrides.value : {},
+      numberOfSeats.value,
+  );
+});
+
+const numberOfSeats = ref<number>(460);
+const useRealSeats = ref<boolean>(true);
+
+model.value = seatDistribution.value;
+watch(() => seatDistribution.value, (newValue) => {
+  model.value = newValue;
+})
+</script>
+
+<template>
+  <div>
+    <NumberOfSeatsPicker
+        :default-number-of-seats="460"
+        :show-real-seats-toggle="Object.keys(mandateOverrides).length > 0"
+        v-model:number-of-seats="numberOfSeats"
+        v-model:use-real-seats="useRealSeats"/>
+  </div>
+</template>
--- src/components/pl/sejm/SejmComponent.vue
+++ src/components/pl/sejm/SejmComponent.vue
@@ -1,50 +1,19 @@
 <script lang="ts" setup>
 import { Sejm } from '@/models/pl/sejm';
-import { computed, ref } from 'vue';
-import NumberOfSeatsPicker from '@/components/pl/sejm/NumberOfSeatsPicker.vue';
-import { getDistrictMagnitude } from '@/utils/pl/district-magnitude';
+import { ref } from 'vue';
 import { NumberMap } from '@/models/utils/number-map';
+import DistrictMagnitudeConfiguration from '@/components/pl/sejm/DistrictMagnitudeConfiguration.vue';
 
 const props = defineProps<{
   data: Sejm
 }>();
 
-const mandateOverrides = computed<NumberMap>(() => {
-  return props.data.districtResults.reduce((acc, districtResult) => {
-    if (districtResult.numberOfMandatesUsed) {
-      acc[districtResult.districtNumber] = districtResult.numberOfMandatesUsed;
-    }
-    return acc;
-  }, {} as NumberMap);
-});
-
-const numberOfSeats = ref<number>(460);
-const useRealSeats = ref<boolean>(true);
-
-const populationMap = computed<NumberMap>(() => {
-  return props.data.districtResults.reduce((acc, districtResult) => {
-    acc[districtResult.districtNumber] = districtResult.population;
-    return acc;
-  }, {} as NumberMap);
-});
-
-const seatDistribution = computed<NumberMap>(() => {
-  return getDistrictMagnitude(
-      populationMap.value,
-      useRealSeats.value ? mandateOverrides.value : {},
-      numberOfSeats.value,
-  );
-});
-
+const seatDistribution = ref<NumberMap>({});
 </script>
 
 <template>
   <div>
-    <NumberOfSeatsPicker
-        :default-number-of-seats="460"
-        :show-real-seats-toggle="Object.keys(mandateOverrides).length > 0"
-        v-model:number-of-seats="numberOfSeats"
-        v-model:use-real-seats="useRealSeats"/>
+    <DistrictMagnitudeConfiguration :data="props.data" v-model="seatDistribution"/>
     {{ seatDistribution }}
   </div>
 </template>
