b24a0a2aac2bcc6bfb1bb662a6ec53a785028c45
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-03-01 16:23:44 +0100

    Cleaner router code

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,33 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    redirect: '/pl/sejm/2023',
-  },
-  {
-    path: '/pl/sejm/:year',
-    component: () => import('./pages/pl/SejmPage.vue'),
-    props: true,
-  },
-  {
-    path: '/:notFoundMatch(.*)',
-    redirect: '/',
-  }
-];
+const redirects: Record<string, string> = {
+  // Redirects to a country, in case no country was passed
+  '/': '/pl',
+  // Redirects to a main page for a country, in case no page was passed
+  '/pl': '/pl/sejm',
+  // Redirects to a most recent year, in case no year was passed
+  '/pl/sejm': '/pl/sejm/2023',
+};
+
+const routes: RouteRecordRaw[] = [];
+
+// Load redirects
+for (const [path, redirect] of Object.entries(redirects)) {
+  routes.push({ path, redirect });
+}
+
+// Load components specific for the elections
+routes.push({
+  path: '/pl/sejm/:year',
+  component: () => import('./pages/pl/SejmPage.vue'),
+  props: true,
+});
+
+// Redirect to a main page in case of a not found page
+routes.push({
+  path: '/:notFoundMatch(.*)',
+  redirect: '/',
+});
 
 const router = createRouter({
   history: createWebHashHistory(),
