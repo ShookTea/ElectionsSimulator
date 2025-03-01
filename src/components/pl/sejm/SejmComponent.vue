6b50748933539271a28120f724c74fe169f908a5
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-03-01 12:28:19 +0100

    Created threshold picker

--- src/components/pl/sejm/SejmComponent.vue
+++ src/components/pl/sejm/SejmComponent.vue
@@ -3,17 +3,26 @@ import { Sejm } from '@/models/pl/sejm';
 import { ref } from 'vue';
 import { NumberMap } from '@/models/utils/number-map';
 import DistrictMagnitudeConfiguration from '@/components/pl/sejm/DistrictMagnitudeConfiguration.vue';
+import ThresholdPicker from '@/components/pl/sejm/ThresholdPicker.vue';
 
 const props = defineProps<{
   data: Sejm
 }>();
 
 const seatDistribution = ref<NumberMap>({});
+const mainThreshold = ref<number>(5);
+const coalitionThreshold = ref<number>(8);
+const nationalMinorityThreshold = ref<number>(0);
 </script>
 
 <template>
   <div>
     <DistrictMagnitudeConfiguration :data="props.data" v-model="seatDistribution"/>
+    <ThresholdPicker
+        v-model:main-threshold="mainThreshold"
+        v-model:coalition-threshold="coalitionThreshold"
+        v-model:national-minority-threshold="nationalMinorityThreshold"
+    />
     {{ seatDistribution }}
   </div>
 </template>
---
+++ src/components/pl/sejm/ThresholdPicker.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const mainThreshold = defineModel<number>('mainThreshold');
+const coalitionThreshold = defineModel<number>('coalitionThreshold');
+const nationalMinorityThreshold = defineModel<number>('nationalMinorityThreshold');
+</script>
+
+<template>
+  <div>
+    <div>
+      <label for="mainThreshold">Main threshold:</label>
+      <input type="number" id="mainThreshold" v-model="mainThreshold" :min="0" :max="100"/>%
+    </div>
+    <div>
+      <label for="coalitionThreshold">Coalition threshold:</label>
+      <input type="number" id="coalitionThreshold" v-model="coalitionThreshold" :min="0" :max="100"/>%
+    </div>
+    <div>
+      <label for="nationalMinorityThreshold">National minority threshold:</label>
+      <input type="number" id="nationalMinorityThreshold" v-model="nationalMinorityThreshold" :min="0" :max="100"/>%
+    </div>
+  </div>
+</template>
