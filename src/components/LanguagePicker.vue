0b1f172a08e854c1c2ab335bc418a34069a60049
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-03-01 18:16:27 +0100

    Storing selected language in local storage

--- src/components/LanguagePicker.vue
+++ src/components/LanguagePicker.vue
@@ -1,4 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+import { watch } from 'vue';
+const { locale } = useI18n();
+watch(locale, (locale) => {
+  document.documentElement.lang = locale;
+  localStorage.setItem('locale', locale);
+});
+</script>
 <template>
   <select v-model="$i18n.locale">
     <option
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,34 @@ import { createI18n } from 'vue-i18n';
 import en from '@/i18n/en';
 import pl from '@/i18n/pl';
 
+function getBestLocales(): string[] {
+  const result = [];
+
+  function insertLanguageCode(code: string): void {
+    if (!result.includes(code)) {
+      result.push(code);
+    }
+    while (code.includes('-')) {
+      code = code.substring(0, code.lastIndexOf('-'));
+      if (!result.includes(code)) {
+        result.push(code);
+      }
+    }
+  }
+  if (localStorage.getItem('locale')) {
+    insertLanguageCode(localStorage.getItem('locale'));
+  }
+  if (navigator.languages) {
+    for (let language of navigator.languages) {
+      insertLanguageCode(language);
+    }
+  }
+  if (navigator.language) {
+    insertLanguageCode(navigator.language);
+  }
+  return result;
+}
+
 const i18n = createI18n({
   locale: 'en',
   fallbackLocale: 'en',
@@ -10,5 +38,15 @@ const i18n = createI18n({
     pl,
   },
 });
+
+for (let locale of getBestLocales()) {
+  // @ts-ignore
+  if (i18n.global.availableLocales.includes(locale)) {
+    // @ts-ignore
+    i18n.global.locale = locale;
+    document.documentElement.lang = locale;
+    break;
+  }
+}
 
 export default i18n;
